3d8f0017ee8664c83196062eef5836c6f692ba81
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2021-02-09 17:04:17 -0600

    Implemented order allocation and related messages and state machine config

---
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationListener.java
@@ -0,0 +1,36 @@
+package guru.sfg.beer.inventory.service.services;
+
+import guru.sfg.beer.inventory.service.config.JMSConfig;
+import guru.sfg.brewery.model.events.AllocateOrderRequest;
+import guru.sfg.brewery.model.events.AllocateOrderResult;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class AllocationListener {
+
+    private AllocationService allocationService;
+    private JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JMSConfig.ALLOCATE_ORDER_QUEUE)
+    public void listen(@Payload AllocateOrderRequest allocateOrderRequest) {
+        AllocateOrderResult.AllocateOrderResultBuilder builder = AllocateOrderResult.builder()
+                .beerOrderDto(allocateOrderRequest.getBeerOrderDto());
+
+        try {
+            Boolean fullyAllocated = allocationService.allocateOrder(allocateOrderRequest.getBeerOrderDto());
+            builder.pendingInventory(!fullyAllocated);
+        } catch (Exception e) {
+            log.error("Allocation failed for order id: " + allocateOrderRequest.getBeerOrderDto().getId());
+            builder.allocationError(true);
+        }
+
+        jmsTemplate.convertAndSend(JMSConfig.ALLOCATE_ORDER_RESPONSE_QUEUE, builder.build());
+    }
+}
